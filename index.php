7ca4d5128b450ffff970d70b36e6b734c42b5518
nerdyash <ythakkar2016@gmail.com>
2017-05-29 16:50:47 -0400

    2nd

--- index.php
+++ index.php
@@ -37,5 +37,103 @@ $myAge = new Age();
 echo "My age is " . $myAge->age . ".<br>";
 
 ?>
+
+<!--Using date function-->
+<?php
+
+$time_hour = date("H");
+
+echo "<p>Time is " .$time_hour."</p>";
+
+if($time_hour < 10){
+    echo "Good Morning!<br>";
+}else if($time_hour >= 10 && $time_hour < 20 ){
+    echo "Have a good day!<br>";
+}else{
+    echo "Good night<br>";
+}
+
+?>
+
+<!--Switch case-->
+<?php
+    $fav_sport = "cricket";
+
+    switch ($fav_sport){
+        case "tennis":
+            echo "My favorite sport is " . $fav_sport . ".<br>";
+            break;
+        case "cricket":
+            echo "My favorite sport is " . $fav_sport . ".<br>";
+            break;
+        default:
+            echo "I do not like sports.<br>";
+    }
+?>
+
+<!--while loop-->
+<?php
+
+    $x = 1;
+    echo "Print odd numbers:<br>";
+    while($x <= 10){
+        //print odd numbers
+        if($x % 2 != 0){
+            echo $x ."<br>";
+        }
+        $x++;
+    }
+
+?>
+
+<!--do while loop-->
+<?php
+    $x = 1;
+    echo "Print even numbers<br>";
+    do{
+        if($x % 2 == 0){
+            echo $x . "<br>";
+        }
+        $x ++;
+    }while($x <= 10);
+?>
+
+<!--Multiplication of two number using function-->
+<?php
+
+    echo "Multiplication of two numbers<br>";
+    function multiplication($x, $y){
+        echo $x . " * " . $y . " = " .($x*$y) . "<br>";
+    }
+
+    multiplication(5, 4);
+    multiplication(14,13);
+
+?>
+
+<!--Arrays-->
+<?php
+
+    $num = [1, 2, 3, 4];
+    $sum = 0;
+    for($x = 0; $x < count($num); $x++){
+        $sum = $sum + $num[$x];
+    }
+    echo "The sum of array is ". $sum . ".<br>" ;
+
+?>
+
+<!--Hashmap or associative array in php-->
+<?php
+
+    $age = ["yash" => 23, "dhruv" => 20];
+    foreach($age as $x => $value){
+        echo "Age of " .$x. " is " .$value . ".<br>";
+    }
+
+?>
+
+
+
 </body>
 </html>
